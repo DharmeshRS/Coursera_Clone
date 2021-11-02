356a5b130634cb580484189ee7af417108b287e8
Surendra9129 <91534930+Surendra9129@users.noreply.github.com>
2021-11-02 22:11:35 +0530

    added drop down list functionality on header element

--- interprises.js
+++ interprises.js
@@ -1,5 +1,4 @@
 let mast_head=document.getElementById("mast-head");
-console.log(mast_head);
 let div1=document.createElement('div');
 let heading=document.createElement('a');
 heading.setAttribute('id','heading')
@@ -18,17 +17,28 @@ home.href="#"
 let dash_line1=document.createElement('a')
 dash_line1.innerText=" |"
 let product=document.createElement('a');
-product.innerText="Product";
-product.href="#"
+product.setAttribute('id','products');
+product.innerText="Products";
+product.href="https://www.coursera.org/business/learn-more/?utm_campaign=website&utm_content=navbar-contact-sales&utm_medium=coursera&utm_source=enterpri"
 product.setAttribute('id','pro')
-product.addEventListener('mouseenter',()=>{
-    // product_list();
+product.addEventListener('mouseover',()=>{
+    product_list();
 })
+product.addEventListener('mouseleave',()=>{
+    product_leave();
+});
 let dash_line2=document.createElement('a')
 dash_line2.innerText=" |"
 let content=document.createElement('a');
+content.setAttribute('id','drop_down_content');
 content.innerText="Content"
-content.href="#";
+content.href="https://www.coursera.org/business/content";
+content.addEventListener('mouseover',()=>{
+    content_list();
+})
+content.addEventListener('mouseleave',()=>{
+    content_leave();
+})
 let dash_line3=document.createElement('a')
 dash_line3.innerText=" |"
 let compare_plan=document.createElement('a');
@@ -38,7 +48,14 @@ let dash_line4=document.createElement('a')
 dash_line4.innerText=" |"
 let resorces=document.createElement('a');
 resorces.innerText="Resourses"
-resorces.href="#";
+resorces.setAttribute('id','drop_down_resources')
+resorces.addEventListener('mouseover',()=>{
+    resources_list()
+});
+resorces.addEventListener('mouseleave',()=>{
+    resources_leave();
+})
+resorces.href="https://www.coursera.org/business/resources";
 let dash_line5=document.createElement('a')
 dash_line5.innerText=" |"
 let Blog=document.createElement('a')
@@ -49,26 +66,101 @@ contact_sales.innerText="Contact sales"
 contact_sales.style.backgroundColor="blue";
 contact_sales.style.color="white";
 contact_sales.style.padding="10px"
-contact_sales.href="#"
+contact_sales.href="https://www.coursera.org/business/learn-more/?utm_campaign=website&utm_content=navbar-contact-sales&utm_medium=coursera&utm_source=enterprise"
 
 div2.append(home,dash_line1, product,dash_line2, content, dash_line3, compare_plan,dash_line4, resorces, dash_line5, Blog,contact_sales);
 
 mast_head.append(div1,div2);
 
-// function product_list(){
-//     let pro=document.getElementById('pro')
-//     pro.innerHTML=null;
-//     let product_drop_down_list=document.createElement("select");
-//     let for_interprises=document.createElement('option');
-//     for_interprises.innerText="for_interprises";
-//     let for_team=document.createElement('option');
-//     for_team.innerText="For Team";
-//     let for_compus=document.createElement('option')
-//     for_compus.innerText="For-compus";
-//     product_drop_down_list.append(for_interprises,for_team,for_compus);
-//     pro.append(product_drop_down_list);
 
-// }
+function product_list(){
+    let pro=document.getElementById('pro')
+    let product_drop_down_list=document.createElement("ul");
+    product_drop_down_list.setAttribute('id','ul_list')
+    let for_interprises=document.createElement('li');
+    for_interprises.innerText="For_interprises";
+    let for_team=document.createElement('li');
+    for_team.innerText="For Team";
+    let for_compus=document.createElement('li')
+    for_compus.innerText="For-compus";
+    let for_government=document.createElement('li')
+     for_government.innerText="For Govenment"
+    product_drop_down_list.append(for_interprises,for_team,for_compus,for_government);
+    pro.append(product_drop_down_list);
+
+}
+
+function product_leave(){
+     let prod=document.getElementById('pro');
+     prod.innerText="Products";  
+  
+}
+
+function content_list(){
+    let con=document.getElementById('drop_down_content')
+    // pro.innerHTML=null;
+    let content_drop_down_list=document.createElement("ul");
+    content_drop_down_list.setAttribute('id','cl_list')
+    let data_acedemy=document.createElement('li');
+    data_acedemy.innerText="Data & Analytics Acedemy";
+    let leadership_acedemy=document.createElement('li');
+    leadership_acedemy.innerText="Leadership Acedemy";
+    let marketing_acedemy=document.createElement('li')
+    marketing_acedemy.innerText="Marketing Acedemy";
+    let Soft_Acedemy=document.createElement('li')
+     Soft_Acedemy.innerText="Software Engineering Acedemy"
+     let Cloud=document.createElement('li');
+     Cloud.innerText="Cloud and IT Acedemy";
+     let glided=document.createElement('li');
+     glided.innerText="Guided Project";
+     let Skill_engi=document.createElement('li')
+      Skill_engi.innerText="Essential Skills Collection for Engineering";
+     let data_science=document.createElement('li')
+      data_science.innerText="Essential Skills Collection for Data Science"
+      let managers=document.createElement('li');
+      managers.innerText="Essential Skills for Managers";
+      let marketing_team=document.createElement('li');
+      marketing_team.innerText="Essential Skill Collection for Marketing Teams";
+      let product_team=document.createElement('li')
+       product_team.innerText="Essential Skills for Product Teams";
+      let finance=document.createElement('li')
+       finance.innerText="Essential Skills Collection for Finance Teams"
+       let sales=document.createElement('li');
+       sales.innerText="Essential Skills Collection for Sales Teams";
+    content_drop_down_list.append(data_acedemy,leadership_acedemy,marketing_acedemy,Soft_Acedemy,Cloud,glided,Skill_engi,data_science,managers,marketing_team,product_team,finance,sales);
+    con.append(content_drop_down_list);
+
+}
+
+function content_leave(){
+     let c=document.getElementById('drop_down_content');
+     c.innerText="Contents";  
+  
+}
+
+function  resources_list(){
+    let res=document.getElementById('drop_down_resources')
+    // pro.innerHTML=null;
+    let resources_drop_down_list=document.createElement("ul");
+    resources_drop_down_list.setAttribute('id','rl_list')
+    let webcast=document.createElement('li');
+    webcast.innerText="Webcast";
+    let ebooks=document.createElement('li');
+    ebooks.innerText="Case Studies & eBooks";
+    let Articles=document.createElement('li')
+     Articles.innerText="Articles & Blog Posts";
+    let conference_hub=document.createElement('li')
+     conference_hub.innerText="Conference Hub 2021"
+    resources_drop_down_list.append(webcast,ebooks,Articles,conference_hub);
+    res.append(resources_drop_down_list);
+
+}
+
+function resources_leave(){
+     let r=document.getElementById('drop_down_resources');
+     r.innerText="Resources";  
+  
+}
 
 let section2=document.getElementById('section2');
 let c_image=document.createElement('img');
@@ -202,52 +294,65 @@ let data_2=[
   let cont_data=[
 
     {
-       title_image:"",
-       title_logo:"",
-        title_heading:"Popular courses from B2B learners in 2021"
+       title_image:"https://www.bing.com/th?id=OIP.0-cIjidOl-A6XLaCoiJC6wHaEK&w=233&h=133&c=8&rs=1&qlt=90&o=6&dpr=1.25&pid=3.1&rm=2",
+       title_logo:"https://www.bing.com/th?id=OIP.0-cIjidOl-A6XLaCoiJC6wHaEK&w=233&h=133&c=8&rs=1&qlt=90&o=6&dpr=1.25&pid=3.1&rm=2",
+        title_heading:"Popular courses from B2B learners in 2021",
+        title_para:"deeplearning This non-technical course will help you understand technologies like machine learning and deep learning to apply AI to problems in your organization. You'll see examples of what today’s AI can and cannot do, how it’s impacting society and how to navigate through these changes."
     },
     {
         title_image:"https://149362230.v2.pressablecdn.com/wp-content/uploads/2019/02/AI-for-everyone-enterprise.png",
         title_logo:"https://149362230.v2.pressablecdn.com/wp-content/uploads/2019/02/DeepLearning_DL.png",
-        title_heading:"AI for Everyone"
-        
+        title_heading:"AI for Everyone",
+        title_para:"deeplearning This non-technical course will help you understand technologies like machine learning and deep learning to apply AI to problems in your organization. You'll see examples of what today’s AI can and cannot do, how it’s impacting society and how to navigate through these changes."
     },
     {
         title_image:"https://149362230.v2.pressablecdn.com/wp-content/uploads/2018/02/CourseTile_NeuralNetworks.jpg",
         title_logo:"https://149362230.v2.pressablecdn.com/wp-content/uploads/2019/02/DeepLearning_DL.png",
-        title_heading:"Neural Networks & Deep Learning"
+        title_heading:"Neural Networks & Deep Learning",
+        title_para:"deeplearning.ai This course covers the ways to build, train, and apply deep neural networks. Featuring case studies from healthcare, autonomous driving, music generation, and natural language processing, learners will master not only the theory of deep learning, but also see how it is applied in industry."
     },
   
     {
         title_image:"https://149362230.v2.pressablecdn.com/wp-content/uploads/2018/02/CourseTile_IllinoisMarketing.jpg",
         title_logo:"https://149362230.v2.pressablecdn.com/wp-content/uploads/2018/02/University-of-Illinois.png",
-        title_heading:"Becoming a Principled and Persuasive Negotiator"
+        title_heading:"Becoming a Principled and Persuasive Negotiator",
+        title_para:"University of Illinois Help your teams understand how digital tools are changing marketing by shifting the balance of power from firms to consumers. One of the most popular courses on Coursera, Marketing in a Digital World is rated by Class Central as one of the Top 50 MOOCs of All Time. This course is part of the iMBA offered by the University of Illinois."
     },
     {
         title_image:"https://149362230.v2.pressablecdn.com/wp-content/uploads/2018/02/CourseTile_YaleNegotiation.jpg",
         title_logo:"https://149362230.v2.pressablecdn.com/wp-content/uploads/2018/02/Yale-University.png",
-        title_heading:"High Performance Collaboration"
+        title_heading:"High Performance Collaboration",
+        title_para:"Yale University This course teaches a winning framework for shaping negotiations with persuasive, principled arguments. Learners will leave the course better able to predict and interpret, and shape the behavior of those they face in competitive situations."
     },
     {
         title_image:"https://149362230.v2.pressablecdn.com/wp-content/uploads/2018/02/CourseTile_NorthwesternHighPerformanceCollab.jpg",
         title_logo:"https://149362230.v2.pressablecdn.com/wp-content/uploads/2018/02/Northwestern.png",
-        title_heading:"Design Thinking for Innovation"
+        title_heading:"Design Thinking for Innovation",
+        title_para:"Northwestern University In this course, your learners will gain essential skills to develop and expand their leadership repertoire. Learners will engage in self-assessments to analyze their leadership style, develop team charters to optimize their groups, and develop a game plan for effective negotiations."
     },
     {
         title_image:"https://149362230.v2.pressablecdn.com/wp-content/uploads/2018/02/CourseTile_UVADesignThinking.jpg",
         title_logo:"https://149362230.v2.pressablecdn.com/wp-content/uploads/2018/02/University-of-Virginia.png",
-        title_heading:"Design Thinking for Innovation"
+        title_heading:"Design Thinking for Innovation",
+        title_para:"University of Virginia Design thinking provides the tools your employees need to think like innovators and uncover creative opportunities. In this course, Professor Jeanne M. Liedtka provides an overview of design thinking and introduces several tools to help learners understand design thinking as a problem solving approach."
     },
     {
         title_image:"https://149362230.v2.pressablecdn.com/wp-content/uploads/2018/02/Home_Course_LearnToLearn.jpg",
         title_logo:"https://149362230.v2.pressablecdn.com/wp-content/uploads/2018/02/UC-San-Diego.png",
-        title_heading:"Learning How to Learn"
+        title_heading:"Learning How to Learn",
+        title_para:"UC San Diego Coursera's most popular course will turbocharge your learners' knowledge acquisition and prime the way for them to master new skills. Covered: illusions of learning, memory techniques, how to deal with procrastination, and best practices for mastering tough subjects."
     }
 ]
 
 cont_data.forEach((el)=>{
     let cont_grid=document.getElementById('cont_grid');
     let title_div=document.createElement('div')
+    title_div.setAttribute('id','title_div')
+    title_div.addEventListener('click',()=>{
+        mouseover(el);
+
+    })
+
     let t_image=document.createElement('img');
     t_image.src=el.title_image
     let t_logo=document.createElement('img');
@@ -264,6 +369,25 @@ cont_data.forEach((el)=>{
     title_div.append(t_image,t_logo,t_heading);
     cont_grid.append(title_div);
 })
+
+
+function mouseover(e){
+       let hover_title=document.getElementById('title_div')
+       let hover_main=document.getElementById('cont_grid')
+       hover_title.innerHTML=null;
+       let hover_heading=document.createElement('h2');
+       hover_heading.innerText=e.title_heading;
+       let hover_para=document.createElement('h6');
+       hover_para.innerText=e.title_para;
+       hover_title.append(hover_heading,hover_para);
+    //    hover_main.append(hover_title);
+       
+}
+
+
+
+
+
 
 let icon_data=[
     "https://149362230.v2.pressablecdn.com/wp-content/uploads/2019/04/pg.png",
