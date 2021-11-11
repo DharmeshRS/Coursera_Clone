4e813eed147deda89e790581af764900ac50d543
Anmolroan <90396759+Anmolroan@users.noreply.github.com>
2021-11-12 01:36:47 +0530

    description page done only get started is left

--- scripts/courseDescription.js
+++ scripts/courseDescription.js
@@ -10,17 +10,32 @@ async function see(){
     console.log(data.elements[0].name);
 var uniName=data.elements[0].name;
 var universityname=document.getElementById("universityname");
-universityname.innerHTML=uniName
+universityname.innerHTML=uniName;
+var puttheuni=document.getElementById("puttheuni");
+puttheuni.innerHTML=uniName;
 }
 see();
 var univImg=document.getElementById("univImg");
 univImg.src=mydata.partnerLogo;
 var courseName=document.getElementById("coursename1");
 courseName.innerHTML=mydata.name;
-var fatborders=document.getElementsByClassName("fatborder")
+var fatborders=document.getElementsByClassName("fatborder");
+// the changing pages;
+// the changing div
+changeingContent=document.getElementById("changeingContent")
+var overview=document.getElementById("overview");
+var admission=document.getElementById("admission")
+var academics=document.getElementById("academics");
+var tution=document.getElementById("tution-main");
+var stu_exp=document.getElementById("stu_exp");
+var careers=document.getElementById("careersd");
+var about_the_university=document.getElementById("about_the_university")
 for(let i =0;i<fatborders.length;i++){
    fatborders[i].onclick=()=>{
        if(i==0){
+           changeingContent.innerHTML=null;
+      changeingContent.innerHTML=overview.innerHTML
+
         fatborders[i].style.borderBottom="4px solid #2a73cc";
         fatborders[i].style.color="#2a73cc";
         fatborders[i].style.fontWeight="600";
@@ -44,6 +59,8 @@ for(let i =0;i<fatborders.length;i++){
         fatborders[5].style.color="black";
        }
        if(i==1){
+        changeingContent.innerHTML=null;
+        changeingContent.innerHTML=admission.innerHTML
         fatborders[i].style.borderBottom="4px solid #2a73cc";
         fatborders[i].style.color="#2a73cc";
         fatborders[i].style.fontWeight="600";
@@ -67,6 +84,8 @@ for(let i =0;i<fatborders.length;i++){
         fatborders[5].style.color="black";
         }
         if(i==2){
+            changeingContent.innerHTML=null;
+            changeingContent.innerHTML=academics.innerHTML
             fatborders[i].style.borderBottom="4px solid #2a73cc";
             fatborders[i].style.color="#2a73cc";
             fatborders[i].style.fontWeight="600";
@@ -90,6 +109,8 @@ for(let i =0;i<fatborders.length;i++){
             fatborders[5].style.color="black";
             }
             if(i==3){
+                changeingContent.innerHTML=null;
+                changeingContent.innerHTML=tution.innerHTML
                 fatborders[i].style.borderBottom="4px solid #2a73cc";
                 fatborders[i].style.color="#2a73cc";
                 fatborders[i].style.fontWeight="600";
@@ -113,6 +134,8 @@ for(let i =0;i<fatborders.length;i++){
                 fatborders[5].style.color="black";
                 }
                 if(i==4){
+                    changeingContent.innerHTML=null;
+                    changeingContent.innerHTML=stu_exp.innerHTML
                     fatborders[i].style.borderBottom="4px solid #2a73cc";
                     fatborders[i].style.color="#2a73cc";
                     fatborders[i].style.fontWeight="600";
@@ -136,6 +159,8 @@ for(let i =0;i<fatborders.length;i++){
                     fatborders[5].style.color="black";
                     }
                     if(i==5){
+                        changeingContent.innerHTML=null;
+                        changeingContent.innerHTML=careers.innerHTML
                         fatborders[i].style.borderBottom="4px solid #2a73cc";
                         fatborders[i].style.color="#2a73cc";
                         fatborders[i].style.fontWeight="600";
@@ -160,7 +185,8 @@ for(let i =0;i<fatborders.length;i++){
                         }
                       
                         if(i==6){
-                            
+                            changeingContent.innerHTML=null;
+                            changeingContent.innerHTML=about_the_university.innerHTML
                            
                             console.log(fatborders[i].textContent);
                             fatborders[i].style.borderBottom="4px solid #2a73cc";
@@ -206,3 +232,10 @@ var max = Math.floor(30);
 var random=Math.floor(Math.random() * (max - min) + min);
 var month2 =document.getElementById("month2")
 month2.innerHTML=random;
+// putting dynamic details
+var putthecoursename=document.getElementById("putcoursename");
+putthecoursename.innerHTML=mydata.name;
+var putthecoursename2=document.getElementById("putcoursename2");
+putthecoursename2.innerHTML=mydata.name;
+var putthecoursename3=document.getElementById("put3");
+putthecoursename3.innerHTML=mydata.name
